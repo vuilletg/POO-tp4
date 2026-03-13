bf31f666eb2ebb940a45a945396d017d7ac8e193
vuilletg <gaspard.vuillet@gmail.com>
2026-03-13 08:24:56 +0100

    init

---
+++ aeroprt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vuilletg on 3/13/26.
+//
+
+#include "aeroprt.h"
---
+++ aeroprt.h
@@ -0,0 +1,14 @@
+//
+// Created by vuilletg on 3/13/26.
+//
+
+#ifndef TP4_AEROPRT_H
+#define TP4_AEROPRT_H
+
+
+class aeroprt {
+
+};
+
+
+#endif //TP4_AEROPRT_H
