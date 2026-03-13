7423af4a88947f4d771842fe483900ec29bda294
vuilletg <gaspard.vuillet@gmail.com>
2026-03-13 10:04:07 +0100

    fin implem aeroport

--- aeroprt.cpp
+++ aeroprt.cpp
@@ -16,12 +16,25 @@ void Aeroprt::write (fstream &stream, std::string delimiter){
     stream << nom << delimiter << id << delimiter << lat << delimiter << lon;
 }
 void Aeroprt::ajouter_connection_entre(int *conection) {
+    entre.resize(++size_e);
     entre.push_back(conection);
 }
 void Aeroprt::ajouter_connection_sortie(int *conection) {
+    entre.resize(++size_s);
     sortie.push_back(conection);
 }
 
 Conexion::Conexion(fstream &stream, std::string delimiter, std::vector<Aeroprt> aeroprt) {
     std::string dep;
+    std::string arr;
+    getLine(stream, dep, delimiter);
+    getLine(stream, dep, delimiter);
+    getLine(stream, duree, delimiter);
+    entree = aeroprt[dep];
+    sortie = aeroprt[arr];
+    entree->ajouter_connection_entre(this);
+    sortie->ajouter_connection_sortie(this);
+}
+void Conexion::write(fstream &stream, std::string delimiter) {
+    stream << entree->id << delimiter << sortie ->id << delimiter << duree;
 }
--- aeroprt.h
+++ aeroprt.h
@@ -9,25 +9,24 @@
 #include <iostream>
 #include <fstream>
 
-class Aeroprt {
+struct Aeroprt {
     std::string nom;
     std::string id;
-    int lat;
-    int lon;
+    int latitude;
+    int longitude;
     std::vector<*Conexion> entre;
     std::vector<*Conexion> sortie;
-public:
     Aeroprt(fstream &stream, std::string delimiter);
     void write (fstream &stream, std::string delimiter);
     void ajouter_connection_entre(*conection);
     void ajouter_connection_sortie(*conection);
-
+    int size_e =0;
+    int size_s =0;
 };
-class Conexion {
+struct Conexion {
     Aeroprt* entree;
     Aeroprt* sortie;
     float duree;
-public:
     Conexion (fstream &stream, std::string delimiter, std::map<std::string ,*Aeroprt> aeroprt);
     void write (fstream &stream, std::string delimiter);
 };
