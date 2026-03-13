d770c7c5f1fcef9eff0534929395a2835fc75f90
vuilletg <gaspard.vuillet@gmail.com>
2026-03-13 09:55:14 +0100

    debut implem

--- aeroprt.cpp
+++ aeroprt.cpp
@@ -1,5 +1,27 @@
 //
 // Created by vuilletg on 3/13/26.
 //
-
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
 #include "aeroprt.h"
+Aeroprt::Aeroprt (fstream &stream, std::string delimiter){
+    getLine(stream, nom, delimiter);
+    getLine(stream, id, delimiter);
+    getLine(stream, lat, delimiter);
+    getLine(stream, lon, delimiter);
+}
+void Aeroprt::write (fstream &stream, std::string delimiter){
+    stream << nom << delimiter << id << delimiter << lat << delimiter << lon;
+}
+void Aeroprt::ajouter_connection_entre(int *conection) {
+    entre.push_back(conection);
+}
+void Aeroprt::ajouter_connection_sortie(int *conection) {
+    sortie.push_back(conection);
+}
+
+Conexion::Conexion(fstream &stream, std::string delimiter, std::vector<Aeroprt> aeroprt) {
+    std::string dep;
+}
--- aeroprt.h
+++ aeroprt.h
@@ -4,10 +4,32 @@
 
 #ifndef TP4_AEROPRT_H
 #define TP4_AEROPRT_H
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
 
+class Aeroprt {
+    std::string nom;
+    std::string id;
+    int lat;
+    int lon;
+    std::vector<*Conexion> entre;
+    std::vector<*Conexion> sortie;
+public:
+    Aeroprt(fstream &stream, std::string delimiter);
+    void write (fstream &stream, std::string delimiter);
+    void ajouter_connection_entre(*conection);
+    void ajouter_connection_sortie(*conection);
 
-class aeroprt {
-
+};
+class Conexion {
+    Aeroprt* entree;
+    Aeroprt* sortie;
+    float duree;
+public:
+    Conexion (fstream &stream, std::string delimiter, std::map<std::string ,*Aeroprt> aeroprt);
+    void write (fstream &stream, std::string delimiter);
 };
 
 
